6f63837b8311c0990c65c052a27f504bf915461a
daniloboccomino <daniloboccomino@gmail.com>
2021-11-16 08:09:54 -0300

    add message tag helper

---
+++ TagHelpers/MessageTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace globalsolution.fomezero.TagHelpers
+{
+    public class MessageTagHelper : TagHelper
+    {
+        public string Text { get; set; }
+        public string Class { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!string.IsNullOrEmpty(Text))
+            {
+                output.TagName = "div";
+                output.Attributes.SetAttribute("class",
+                    string.IsNullOrEmpty(Class) ? "alert alert-success" : Class);
+                output.Content.SetContent(Text);
+            }
+        }
+    }
+}
